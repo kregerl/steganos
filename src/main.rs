84f02a6b939135e88c664a5d86acbe478a11cdd7
kregerl <loucakreger@gmail.com>
2022-06-29 12:31:28 -0400

    Added a function to turn an image into bits and write those bits into another image. Can be read the same as if text was written in the image.

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,8 @@ fn main() {
     stegnog [OPTIONS]
 OPTIONS:
     -in <original_png> -out <output_png> 
-    -read <input_png>";
+    -read <input_png>
+	-in <original_png> -out <output_png> -embed <embed_png>";
 
     if args.len() == 5 {
         println!("{:?}", args);
@@ -31,9 +32,61 @@ OPTIONS:
 
             println!("{}", read_image(img));
         }
-    } else {
+    } else if args.len() == 7 {
+		if args[1] == "in" && args[3] == "out" && args[5] == "embed" {
+			let img = image::open(&args[2]).unwrap();
+			let embed = image::open(&args[6]).unwrap();
+			
+			let bits = convert_image_to_bits(embed);
+			write_bits_to_image(img, &args[4], bits);		
+
+		}
+	} else {
         println!("{}", usage);
     }
+}
+
+fn write_bits_to_image(image: DynamicImage, output_name: &str, bits: Vec<u8>) {
+    let mut bit_iter = bits.iter();
+    let (width, height) = image.dimensions();
+    let mut new_image = RgbaImage::new(width, height);
+    let mut testing: Vec<u8> = Vec::new();
+    for (x, y, mut pixel) in image.pixels() {
+        pixel = pixel.map(|color| {
+            if let Some(bit) = bit_iter.next() {
+                testing.push((color & 254) | bit);
+                println!("Mapped: {} to {}", color, (color & 254) | bit);
+                return (color & 254) | bit;
+            }
+            color
+        });
+        new_image.put_pixel(x, y, pixel);
+    }
+
+    let res = image::save_buffer(&Path::new(output_name), &*new_image, width, height, ColorType::Rgba8);
+    match res {
+        Err(err) => eprintln!("{}", err),
+        _ => {}
+    }
+}
+
+fn convert_image_to_bits(image: DynamicImage) -> Vec<u8> {
+	let mut bytes: Vec<u8> = Vec::new();
+	for (_, _, pixel) in image.pixels() {
+		for color in pixel.0 {
+			bytes.push(color);
+		}
+	}
+	let mut bits: Vec<u8> = Vec::new();
+	let mut reader = BitReader::new(&*bytes);
+    while reader.remaining() > 0 {
+        let mask = 0 | reader.read_u8(1).unwrap();
+        bits.push(mask);
+    }
+    for _ in 0..8 {
+        bits.push(0);
+    }
+	bits
 }
 
 fn read_image(image: DynamicImage) -> String {
@@ -54,9 +107,8 @@ fn read_image(image: DynamicImage) -> String {
         }
     }
     let mut count = 0;
-    for bit in bits {
-        print!("{}", bit);
-        if count != 0 && count % 8 == 0 {
+    for (i, bit) in bits.iter().enumerate() {
+        if i != 0 && i % 8 == 0 {
             println!();
         }
         count += 1;
