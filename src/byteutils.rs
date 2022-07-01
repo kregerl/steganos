9d06ed8a6fa8c7ffb9729c6d92b53e132579c2f4
kregerl <loucaskreger@gmail.com>
2022-06-30 22:39:58 -0400

    Added decoding the messages

--- src/byteutils.rs
+++ src/byteutils.rs
@@ -1,36 +1,48 @@
-// TODO: Refactor this to accept the actual thing its encoding and have a function for getting it as bytes.
-// Allows for the header to be placed in the bytes along with the 0b00000000 separator.
+use image::{DynamicImage, GenericImageView};
+
 pub enum EncodedType {
     Text(String),
-    RgbaPng,
+    RgbaPng(DynamicImage),
 }
 
 impl EncodedType {
     fn header(&self) -> &str {
         match *self {
-            EncodedType::Text(_) => "text",
-            EncodedType::RgbaPng => "rgba"
+            EncodedType::Text(_) => "txt",
+            EncodedType::RgbaPng(_) => "img"
         }
     }
 
     fn byte_header(&self) -> Vec<Byte> {
         self.header().bytes().map(|c| Byte::new(c)).collect()
     }
+
     pub fn to_bytes(&self) -> Vec<Byte> {
         let mut bytes = self.byte_header();
-        bytes.push(Byte::zero());
         match self {
             EncodedType::Text(s) => {
                 for b in s.bytes() {
                     bytes.push(Byte::new(b));
                 }
             }
-            EncodedType::RgbaPng => {}
+            EncodedType::RgbaPng(image) => {
+                for (_, _, pixel) in image.pixels() {
+                    for color in pixel.0 {
+                        bytes.push(Byte::new(color));
+                    }
+                }
+            }
         }
+        bytes.push(Byte::zero());
         bytes
+    }
+
+    pub fn to_bits(&self) -> Vec<bool> {
+        self.to_bytes().iter().map(|b| b.get_bits()).flatten().collect()
     }
 }
 
+#[derive(PartialEq, Clone)]
 pub struct Byte {
     byte: u8,
 }
@@ -60,6 +72,17 @@ impl Byte {
         bits.reverse();
         bits
     }
+
+    pub fn as_char(&self) -> char {
+        self.byte as char
+    }
+
+    pub fn print_bits(&self) {
+        for bit in self.get_bits() {
+            print!("{}", bit as u8);
+        }
+        println!();
+    }
 }
 
 impl From<char> for Byte {
@@ -70,13 +93,34 @@ impl From<char> for Byte {
     }
 }
 
+impl From<&[bool]> for Byte {
+    fn from(arr: &[bool]) -> Self {
+        if arr.len() > 8 {
+            panic!("Cannot form a byte from a bool array of size: {}", arr.len());
+        }
+        let mut byte_str: String = String::new();
+        for b in arr {
+            byte_str.push_str(&*(*b as u8).to_string());
+        }
+
+
+        match u8::from_str_radix(&byte_str[..], 2) {
+            Ok(byte) => {
+                Self {
+                    byte
+                }
+            }
+            Err(err) => {
+                eprintln!("Error parsing integer, byte is 0. {}", err);
+                Self {
+                    byte: 0
+                }
+            }
+        }
+    }
+}
+
 pub fn get_lsb(byte: u8) -> bool {
     byte & 1 != 0
 }
-
-
-
-
-
-
 
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,10 @@ use std::io::{Write, stdin, stdout};
 use std::env;
 use std::path::Path;
 use image::{RgbaImage, GenericImageView, DynamicImage, Pixel, ColorType, Rgba};
-use bitreader::BitReader;
-use crate::byteutils::{Byte, EncodedType};
+use crate::byteutils::{Byte, EncodedType, get_lsb};
 
+// TODO: Switch cmdline args to CLAP https://rust-lang-nursery.github.io/rust-cookbook/cli/arguments.html
 fn main() {
-    println!("Hello, world!");
-
     let args: Vec<String> = env::args().collect();
 
     let usage = "USAGE:
@@ -24,135 +22,125 @@ OPTIONS:
         if args[1] == "in" && args[3] == "out" {
             let img = image::open(&args[2]).unwrap();
 
-            const TEXT: &str = "Testing byte struct";
+            const TEXT: &str = "           __
+             <(o )___
+              ( ._> /
+               `---'  ";
 
-            write_image(img, TEXT, &args[4]);
+            embed_text_in_image(img, TEXT, &args[4]);
         }
     } else if args.len() == 3 {
         if args[1] == "read" {
             let img = image::open(&args[2]).unwrap();
 
-            println!("{}", read_image(img));
+            read_image(img);
         }
     } else if args.len() == 7 {
-        if args[1] == "in" && args[3] == "out" && args[5] == "embed" {
-            let img = image::open(&args[2]).unwrap();
-            let embed = image::open(&args[6]).unwrap();
-
-            let bits = convert_image_to_bits(embed);
-            write_bits_to_image(img, &args[4], bits);
-        }
+        // if args[1] == "in" && args[3] == "out" && args[5] == "embed" {
+        //     let img = image::open(&args[2]).unwrap();
+        //     let embed = image::open(&args[6]).unwrap();
+        //
+        //     let bits = convert_image_to_bits(embed);
+        //     write_bits_to_image(img, &args[4], bits);
+        // }
     } else {
         println!("{}", usage);
     }
 }
 
-fn write_bits_to_image(image: DynamicImage, output_name: &str, bits: Vec<u8>) {
-    let mut bit_iter = bits.iter();
-    let (width, height) = image.dimensions();
-    let mut new_image = RgbaImage::new(width, height);
-    let mut testing: Vec<u8> = Vec::new();
-    for (x, y, mut pixel) in image.pixels() {
-        pixel = pixel.map(|color| {
-            if let Some(bit) = bit_iter.next() {
-                testing.push((color & 254) | bit);
-                println!("Mapped: {} to {}", color, (color & 254) | bit);
-                return (color & 254) | bit;
-            }
-            color
-        });
-        new_image.put_pixel(x, y, pixel);
-    }
+fn read_image(image: DynamicImage) {
+    let mut encoded_contents: Vec<bool> = Vec::new();
 
-    let res = image::save_buffer(&Path::new(output_name), &*new_image, width, height, ColorType::Rgba8);
-    match res {
-        Err(err) => eprintln!("{}", err),
-        _ => {}
-    }
-}
-
-fn convert_image_to_bits(image: DynamicImage) -> Vec<u8> {
-    let mut bytes: Vec<u8> = Vec::new();
-    for (_, _, pixel) in image.pixels() {
-        for color in pixel.0 {
-            bytes.push(color);
-        }
-    }
-    let mut bits: Vec<u8> = Vec::new();
-    let mut reader = BitReader::new(&*bytes);
-    while reader.remaining() > 0 {
-        let mask = 0 | reader.read_u8(1).unwrap();
-        bits.push(mask);
-    }
-    for _ in 0..8 {
-        bits.push(0);
-    }
-    bits
-}
-
-fn read_image(image: DynamicImage) -> String {
-    let mut bits: Vec<u8> = Vec::new();
-    let mut num_of_consecutive_zeros = 0;
-    let mut consecutive_zeros = 0;
+    let mut num_consecutive_zeros = 0;
     'outer: for (_, _, pixel) in image.pixels() {
         for color in pixel.0 {
-            let bit = color & 1;
-            if bit == 0 {
-                consecutive_zeros += 1;
+            let bit = get_lsb(color);
+            if bit {
+                num_consecutive_zeros = 0;
             } else {
-                consecutive_zeros = 0;
+                num_consecutive_zeros += 1;
             }
-            bits.push(bit);
-            if consecutive_zeros == 8 {
-                num_of_consecutive_zeros += 1;
-            }
-            if num_of_consecutive_zeros >= 2 {
+            encoded_contents.push(bit);
+            if num_consecutive_zeros == 8 {
                 break 'outer;
             }
         }
     }
-    for byte in bits.chunks(8) {
-        for bit in byte {
-            print!("{}", bit);
+
+    let mut bytes: Vec<Byte> = encoded_contents.chunks(8).map(|chunk| Byte::from(chunk)).collect();
+
+    let body = bytes.split_off(3);
+    let header: String = bytes.iter().map(|c| c.as_char()).collect();
+
+    match header.as_str() {
+        "txt" => { println!("Decoded message: \n\t{}", read_bytes_as_text(body)) }
+        "img" => {
+            read_bytes_as_image(body);
         }
-        println!();
+        _ => {
+            println!("Unknown header type, dumping output.");
+            for byte in bytes[3..].iter() {
+                byte.print_bits();
+            }
+        }
     }
-    String::from("")
 }
 
-fn write_image(original: DynamicImage, text: &str, output_name: &str) {
-    let bytes = EncodedType::Text(String::from(text)).to_bytes();
-    let mut bits: Vec<bool> = Vec::new();
-    for byte in bytes {
-        bits.append(&mut byte.get_bits());
-    }
-    bits.append(&mut Byte::zero().get_bits());
-    println!("-------------------------");
-    for b in &bits {
-        print!("{}", b);
-    }
-    println!();
+fn embed_text_in_image(original: DynamicImage, text: &str, output_path: &str) {
+    let bits = EncodedType::Text(String::from(text)).to_bits();
+    // let bits: Vec<bool> = bytes.iter().map(|b| b.get_bits()).flatten().collect();
 
+    let new_image = write_bits_to_image(original, bits);
+
+    save_image(new_image, output_path);
+}
+
+fn embed_image_in_image(original: DynamicImage, image_to_embed: DynamicImage, output_path: &str) {
+    let bits = EncodedType::RgbaPng(image_to_embed).to_bits();
+
+    let new_image = write_bits_to_image(original, bits);
+
+    save_image(new_image, output_path);
+}
+
+fn write_bits_to_image(original_image: DynamicImage, bits: Vec<bool>) -> RgbaImage {
     let mut bit_iter = bits.iter();
-    let (width, height) = original.dimensions();
-    let mut new_image = RgbaImage::new(width, height);
-    let mut testing: Vec<u8> = Vec::new();
-    for (x, y, mut pixel) in original.pixels() {
+    let (width, height) = original_image.dimensions();
+    let mut image = RgbaImage::new(width, height);
+    for (x, y, mut pixel) in original_image.pixels() {
         pixel = pixel.map(|color| {
-            if let Some(bit_bool) = bit_iter.next() {
-                let bit = if *bit_bool { 1 } else { 0 };
-                testing.push((color & 254) | bit);
-                println!("Mapped: {} to {}", color, (color & 254) | bit);
-                return (color & 254) | bit;
+            if let Some(bit) = bit_iter.next() {
+                return (color & 254) | *bit as u8;
             }
             color
         });
-        new_image.put_pixel(x, y, pixel);
+        image.put_pixel(x, y, pixel);
+    }
+    image
+}
+
+fn read_bytes_as_text(bytes: Vec<Byte>) -> String {
+    let mut result = String::new();
+
+    for byte in bytes {
+        if Byte::zero() != byte {
+            result.push(byte.as_char());
+        }
     }
 
-    let res = image::save_buffer(&Path::new(output_name), &*new_image, width, height, ColorType::Rgba8);
-    match res {
-        Err(err) => eprintln!("{}", err),
-        _ => {}
+    result
+}
+
+fn read_bytes_as_image(bytes: Vec<Byte>) {
+    todo!("Save the image's dimensions so it can be read again. Otherwise the size of the image is unknown.")
+}
+
+fn save_image(image: RgbaImage, output_path: &str) {
+    let (width, height) = image.dimensions();
+    let result = image::save_buffer(&Path::new(output_path), &*image, width, height, ColorType::Rgba8);
+    match result {
+        Ok(_) => { println!("Wrote image to {}", output_path); }
+        Err(err) => { eprintln!("Error writing image to path: {}", err); }
     }
 }
+
